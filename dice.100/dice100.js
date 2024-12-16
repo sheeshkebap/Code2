ae0e74b6d98fc7f89c4730722040e50cf21999a4
sheeshkebap <andr.niederreiter@gmail.com>
2024-12-16 17:18:24 +0100

    dice 100

--- dice.100/dice100.js
+++ dice.100/dice100.js
@@ -4,7 +4,20 @@ exports.andi = void 0;
 var andi;
 (function (andi) {
     function strategy(_score, _active, _potential) {
-        ;
+        if (_score[0] >= 80) {
+            return true;
+        }
+        else {
+            if (_score[1] > _score[0]) {
+                return Math.random() < 0.3;
+            }
+            else {
+                if (_score[1] > _score[0] + 40) {
+                    return true;
+                }
+            }
+            // return Math.random() < 0.7
+        }
     }
     andi.strategy = strategy;
 })(andi || (exports.andi = andi = {}));
--- dice.100/dice100.ts
+++ dice.100/dice100.ts
@@ -1,5 +1,17 @@
 export namespace andi {
     export function strategy (_score: number[], _active: number, _potential: number): boolean {
-        ;
+       
+       if (_score[0] >= 80) {
+            return true;
+       } else {
+        if (_score[1] > _score[0]) {
+            return Math.random() < 0.3;
+        } else {
+            if (_score[1] > _score[0] + 40) {
+                return true
+            }
+        }
+        // return Math.random() < 0.7
+       }
     }
 }
