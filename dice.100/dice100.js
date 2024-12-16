640de2c674201081164fdff9da4bff38ebb2800f
sheeshkebap <andr.niederreiter@gmail.com>
2024-12-16 16:59:30 +0100

    dice 100

--- dice.100/dice100.js
+++ dice.100/dice100.js
@@ -1,1 +1,10 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.andi = void 0;
+var andi;
+(function (andi) {
+    function strategy(_score, _active, _potential) {
+        ;
+    }
+    andi.strategy = strategy;
+})(andi || (exports.andi = andi = {}));
