360240ad2f8b99946a96b227412fdbc34bc0e096
sheeshkebap <andr.niederreiter@gmail.com>
2024-12-16 17:26:36 +0100

    dice100

--- dice.100/dice100.js
+++ dice.100/dice100.js
@@ -1,18 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.andi = void 0;
-var andi;
+export var andi;
 (function (andi) {
     function strategy(_score, _active, _potential) {
-        if (_score[0] >= 80) {
+        if (_score[_active] >= 80) {
             return true;
         }
         else {
-            if (_score[1] > _score[0]) {
+            if (_score[1 - _active] > _score[_active]) {
                 return Math.random() < 0.3;
             }
             else {
-                if (_score[1] > _score[0] + 40) {
+                if (_score[1 - _active] > _score[_active] + 40) {
                     return true;
                 }
                 else {
@@ -22,4 +19,4 @@ var andi;
         }
     }
     andi.strategy = strategy;
-})(andi || (exports.andi = andi = {}));
+})(andi || (andi = {}));
--- dice.100/dice100.ts
+++ dice.100/dice100.ts
@@ -1,13 +1,13 @@
 export namespace andi {
     export function strategy (_score: number[], _active: number, _potential: number): boolean {
        
-       if (_score[0] >= 80) {
+       if (_score[_active] >= 80) {
             return true;
        } else {
-        if (_score[1] > _score[0]) {
+        if (_score[1 - _active] > _score[_active]) {
             return Math.random() < 0.3;
         } else {
-            if (_score[1] > _score[0] + 40) {
+            if (_score[1 - _active] > _score[_active] + 40) {
                 return true
             } else {
                  return Math.random() < 0.5
